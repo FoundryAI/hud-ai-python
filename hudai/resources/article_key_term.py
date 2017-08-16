8ca08c204cb27d6dfc07c931da75df3d1a442ccc
Jonathan Bender <jlbender@gmail.com>
2017-08-16 12:59:48 -0400

    article key terms are child objects of an article

--- hudai/resources/article_key_term.py
+++ hudai/resources/article_key_term.py
@@ -6,17 +6,23 @@ from ..resource import Resource
 
 class ArticleKeyTermResource(Resource):
     def __init__(self, client):
-        Resource.__init__(self, client, base_path='/article-key-terms')
+        Resource.__init__(self, client, base_path='/{article_id}/key-terms')
         self.resource_name = 'ArticleKeyTerm'
 
-    def list(self, term=None, article_id=None, page=None):
-        return self._list(term=term, article_id=article_id, page=page)
+    def list(self, article_id, page=None):
+        query_params = self._set_limit_offset({'page': page})
 
-    def create(self, term=None, article_id=None):
-        return self._create(term=term, article_id=article_id)
+        return self.http_get('/', params={'article_id': article_id},
+                                  query_params=query_params)
 
-    def fetch(self, entity_id):
-        return self._fetch(entity_id)
+    def create(self, article_id, term):
+        return self.http_post('/', params={'article_id': article_id},
+                                   data={'term': term})
 
-    def delete(self, entity_id):
-        return self._delete(entity_id)
+    def fetch(self, article_id, term):
+        return self.http_get('/{term}',
+                             params={'article_id': article_id, 'term': term})
+
+    def delete(self, article_id, term):
+        return self.http_delete('/{term}',
+                                params={'article_id': article_id, 'term': term})
