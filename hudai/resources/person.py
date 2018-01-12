7933dd01818091951c27e170339d07a9a0689e80
Peter Britton <peter@foundry.ai>
2018-01-12 12:59:19 -0500

    add company_id

--- hudai/resources/person.py
+++ hudai/resources/person.py
@@ -16,25 +16,27 @@ class PersonResource(Resource):
                           twitter_handle=twitter_handle,
                           page=page)
 
-    def create(self, name=None, title=None, twitter_handle=None, linked_in_url=None, image_url=None, source=None):
+    def create(self, name=None, title=None, twitter_handle=None, linked_in_url=None, image_url=None, source=None, company_id=None):
         return self._create(name=name,
                             title=title,
                             twitter_handle=twitter_handle,
                             linked_in_url=linked_in_url,
                             image_url=image_url,
-                            source=source)
+                            source=source,
+                            company_id=company_id)
 
     def fetch(self, entity_id):
         return self._fetch(entity_id)
 
-    def update(self, entity_id, name=None, title=None, twitter_handle=None, linked_in_url=None, image_url=None, source=None):
+    def update(self, entity_id, name=None, title=None, twitter_handle=None, linked_in_url=None, image_url=None, source=None, company_id=None):
         return self._update(entity_id,
                             name=name,
                             title=title,
                             twitter_handle=twitter_handle,
                             linked_in_url=linked_in_url,
                             image_url=image_url,
-                            source=source)
+                            source=source,
+                            company_id=company_id)
 
     def delete(self, entity_id):
         return self._delete(entity_id)
