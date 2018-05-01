585910ad9276618e8be7ade4e39a0cde64910c99
Peter Britton <peter@foundry.ai>
2018-05-01 11:54:36 -0400

    create list method for feed

---
+++ hudai/resources/feed.py
@@ -0,0 +1,45 @@
+from ..helpers.resource import Resource
+
+
+class FeedResource(Resource):
+    def __init__(self, client):
+        Resource.__init__(self, client, base_path='/users/feed')
+        self.resource_name = 'Feed'
+
+    def list(
+        self,
+        userId,
+        companyIds=None,
+        keyTerms=None,
+        maxImportance=None,
+        maxRelevance=None,
+        minImportance=None,
+        minRelevance=None,
+        publishedAfter=None,
+        publishedBefore=None,
+        scoredAfter=None,
+        scoredBefore=None,
+        sourceIds=None,
+        tags=None,
+        text=None,
+        types=None,
+        weights=None,
+    ):
+        return self._list(
+            userId=userId,
+            companyIds=companyIds,
+            keyTerms=keyTerms,
+            maxImportance=maxImportance,
+            maxRelevance=maxRelevance,
+            minImportance=minImportance,
+            minRelevance=minRelevance,
+            publishedAfter=publishedAfter,
+            publishedBefore=publishedBefore,
+            scoredAfter=scoredAfter,
+            scoredBefore=scoredBefore,
+            sourceIds=sourceIds,
+            tags=tags,
+            text=text,
+            types=types,
+            weights=weights,
+        )
