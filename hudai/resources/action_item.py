81df8db72a312d6dea07bda4a11e5908a9228e63
Peter Britton <peter@foundry.ai>
2018-06-19 18:23:40 -0400

    add dismissed, completed and archived options

--- hudai/resources/action_item.py
+++ hudai/resources/action_item.py
@@ -15,6 +15,15 @@ class ActionItemResource(Resource):
                    user_id=None,
                    company_ids=None,
                    person_ids=None,
+                   completed=None,
+                   completed_before=None,
+                   completed_after=None,
+                   dismissed=None,
+                   dismissed_before=None,
+                   dismissed_after=None,
+                   archived=None,
+                   archived_before=None,
+                   archived_after=None,
                    page=None):
         return self._list(created_after=created_after,
                           created_before=created_before,
@@ -22,6 +31,15 @@ class ActionItemResource(Resource):
                           user_id=user_id,
                           company_ids=company_ids,
                           person_ids=person_ids,
+                          completed=completed,
+                          completed_before=completed_before,
+                          completed_after=completed_after,
+                          dismissed=dismissed,
+                          dismissed_before=dismissed_before,
+                          dismissed_after=dismissed_after,
+                          archived=archived,
+                          archived_before=archived_before,
+                          archived_after=archived_after,
                           page=page)
 
     def create(self, user_id=None,
@@ -51,6 +69,7 @@ class ActionItemResource(Resource):
                      associated_entity_id=None,
                      completed_at=None,
                      dismissed_at=None,
+                     archived_at=None,
                      propensity_score=None):
         return self._update(entity_id,
                             user_id=user_id,
@@ -59,6 +78,7 @@ class ActionItemResource(Resource):
                             associated_entity_id=associated_entity_id,
                             completed_at=completed_at,
                             dismissed_at=dismissed_at,
+                            archived_at=archived_at,
                             propensity_score=propensity_score)
 
     def delete(self, entity_id):
